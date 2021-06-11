e1a6cd9ad76372cfbf667b12986b0fd151787d0d
EliasOPrado2 <elias.prado@samplemed.com.br>
2021-06-10 22:54:25 -0300

    initial commit

---
+++ src/List.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+  return (
+    <div>
+      <h2>List</h2>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
